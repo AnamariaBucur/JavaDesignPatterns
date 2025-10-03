8a729c79b0205632c153c0fc3e78987f9761ef62
AnamariaBucur <anamaria.bucur000@gmail.com>
2025-10-03 21:08:45 +0300

    Observer pattern exercises

---
+++ src/com/BehavioralDesignPatterns/Observer/WeatherWarnings/App.java
@@ -0,0 +1,18 @@
+package com.BehavioralDesignPatterns.Observer.WeatherWarnings;
+
+public class App {
+    public static void main(String[] args) {
+
+        var location1 = new Location();
+        var location2 = new Location();
+        var weatherUpdates = new WeatherUpdates();
+
+        location1.addPropertyChangeListener(weatherUpdates);
+        location2.addPropertyChangeListener(weatherUpdates);
+
+        location1.setWeatherEvent("Risk of flooding");
+        location2.setWeatherEvent("Risk of drought");
+
+        weatherUpdates.printUpdates();
+    }
+}
